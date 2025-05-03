3a363919016329b4fa7ff1bfe796b1d67597d06d
harpreet kaur gothra <harpreetk2146@gmail.com>
2025-05-03 23:57:52 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,16 +3,16 @@ const router=express.Router();
 
 //only admin can access this router
 router.get("/admin", (req, res) => {
-    res.send("Admin access granted");
+    res.send("Admin access granted<3");
 });
 //both admin and manager can access this router
 router.get("/manager", (req, res) => {
-    res.send("Manager access granted");
+    res.send("Manager access granted<3");
 });
 
 //all can access this router
 router.get("/user", (req, res) => {
-    res.send("User access granted");
+    res.send("User access granted<3");
 });
 
 module.exports=router;
