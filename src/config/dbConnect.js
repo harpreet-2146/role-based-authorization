f04d63288707e228b1b4f046d7dea9163ff77472
harpreet kaur gothra <harpreetk2146@gmail.com>
2025-05-03 23:57:11 +0530

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,8 @@
 const mongoose=require('mongoose');
 
-const dbConnect=async()=>{
+const dbConnect=async()=>{ //async&await
     try{
-    console.log("Connection string:", process.env.CONNECTION_STRING);
+    console.log("Connection string:", process.env.CONNECTION_STRING); 
     const connect=await mongoose.connect(process.env.CONNECTION_STRING)
  console.log(`Database connected:${connect.connection.host},${connect.connection.name}`)
 }catch(err){
